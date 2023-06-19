3e30169d3285c03a5163a70250b19cf320280cc0
schout6 <schout6@gmail.com>
2023-06-19 03:03:42 -0400

    Update authenticator.py

--- authenticator.py
+++ authenticator.py
@@ -5,6 +5,7 @@ from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -20,54 +21,11 @@ def hash_password(password):
 # Add your code here
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # Retrieve form data
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        
-        # Perform form validation
-        errors = []
-        if not username:
-            errors.append('Username is required.')
-        if not email:
-            errors.append('Email is required.')
-        if not password:
-            errors.append('Password is required.')
-        
-        # Perform additional form validation (e.g., password strength, email format)
-        # Add appropriate error messages to the 'errors' list
-        
-        if errors:
-            return render_template('register.html', errors=errors)
-        
-        # Hash the password before storing it
-        hashed_password = hash_password(password)
-        
-        # Save user data to the in-memory storage
-        users.append({'username': username, 'email': email, 'password': hashed_password})
-        
-        return "Registration successful!"
-    else:
-        return render_template('register.html')
+    # Rest of the code...
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # Retrieve form data
-        username = request.form['username']
-        password = request.form['password']
-        
-        # Find the user in the in-memory storage
-        user = next((user for user in users if user['username'] == username), None)
-        
-        # Check if the user exists and validate the password
-        if user and hash_password(password) == user['password']:
-            return "Login successful!"
-        else:
-            return render_template('login.html', error="Invalid username or password.")
-    else:
-        return render_template('login.html')
+    # Rest of the code...
 
 if __name__ == '__main__':
     app.run(debug=True)
